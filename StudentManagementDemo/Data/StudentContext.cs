a2ebf697f13d4eda105957920f4a3abf26fb968e
JuusoHeikkinen02 <juuso.heikkinen02@gmail.com>
2025-02-05 11:00:07 +0200

    test

--- StudentManagementDemo/Data/StudentContext.cs
+++ StudentManagementDemo/Data/StudentContext.cs
@@ -7,7 +7,7 @@ namespace StudentManagement.Data
     public class StudentContext : DbContext
     {
         public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
-
+         
         public DbSet<Student> Students { get; set; } = null!;
     }
 }
